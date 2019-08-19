daf1dad9aca5729ce990d8512d4ac3da60cd809c
mostafa-raafat <mraafatsaad94@gmail.com>
2019-08-20 00:18:45 +0200

    init an app

---
+++ app/scripts/main.js
@@ -0,0 +1,28 @@
+console.log('\'Allo \'Allo!');
+
+// Uncomment to enable Bootstrap tooltips
+// https://getbootstrap.com/docs/4.0/components/tooltips/#example-enable-tooltips-everywhere
+// $(function () { $('[data-toggle="tooltip"]').tooltip(); });
+
+// Uncomment to enable Bootstrap popovers
+// https://getbootstrap.com/docs/4.0/components/popovers/#example-enable-popovers-everywhere
+// $(function () { $('[data-toggle="popover"]').popover(); });
+
+$('.carousel').carousel();
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
